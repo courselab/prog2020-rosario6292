05d22d03d7d76d721b46778927e2332acd6de78f
rosario6292 <rosariowang78@gmail.com>
2020-06-25 16:50:58 +0000

    entrega 1

--- m001.c
+++ m001.c
@@ -22,14 +22,16 @@
 
 /* Return the greatest among a, b and c. */
 
-int greatest(int a, int b, int c){
-    if (a > b && a > c)
-        return a;
-    else if(b > a && b > c)
-            return b;
-        else
-            if(c > a && c > b)
-                return c;
+int greatest(int a, int b, int c)
+{
+    if (a > b && a > c){
+      return a;
+    }
+    else if (b > a && b > c){
+      return b;
+    }
+      else
+      return c;
 }
 /* Do not edit this function. */
 
