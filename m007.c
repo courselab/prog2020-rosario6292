5ca766889717f76cffa6961f30f1c000d68815e6
rosario6292 <rosariowang78@gmail.com>
2020-06-26 20:16:31 +0000

    m007

--- m007.c
+++ m007.c
@@ -32,10 +32,14 @@ int lettercount (char *s)
   n=strlen(s);
   for(i=0; i<n; i++)
   {
+    if(s[i] == '1' || s[i] == '2' || s[i] == '3' || s[i] == '4' || s[i] == '5' || s[i] == '6' || s[i] == '7' || s[i] == '8' || s[i] == '9' || s[i] == '0'){
+    n = n-1;
+    }
     if(s[i] == ' '){
     n = n-1;
     }
   }
+  n = n-1;
   return n;
 }
 
